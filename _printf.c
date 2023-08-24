b214fa226089788e2448a95f26ea2a87e197de29
Gaily-UI <ifejika.abigail@gmail.com>
2023-08-24 02:45:45 -0700

    specifiers

--- _printf.c
+++ _printf.c
@@ -1,95 +1,103 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
-/**
- * _printf - Printf function
- * @format: array of characters
- * Return: Always 0
- */
+
 int _printf(const char *format, ...)
 {
-	
-	int lp = 0;
-	int i;
-	va_list args;
-	va_start(args, format);
-	if (format == NULL)
-	{
-		return (-1);
-	}
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (!*format) 
-			{
+    int i, tf;
+    tf = 0;
 
-        			return -1;
-			}
-			switch (*format)
-			{
-				case 'c':
-					lp += printf("%c", va_arg(args, int));
-					break;
-				case 's':
-					lp += printf("%s", va_arg(args, char *));
-					break;
-				case 'd':
-				case 'i':
-					lp += printf("%d", va_arg(args, int));
-					break;
-				case 'u':
-					lp += printf("%u", va_arg(args, unsigned int));
-					break;
-				case 'o':
-					lp += printf("%o", va_arg(args, unsigned int));
-					break;
-				case 'x':
-					lp += printf("%x", va_arg(args, int));
-					break;
-				case 'X':
-					lp += printf("%X", va_arg(args, int));
-					break;
-				case 'p':
-					lp += printf("%p", va_arg(args, void *));
-					break;
-				case 'b':
-					{
-					unsigned int num = va_arg(args, unsigned int);
-					char buffer[32];
-					int index = 0;
-					if (num == 0) {
-					return printf("0");
-					}
-					else 
-					{
-						while (num > 0)
-						{
-							buffer[index++] = num % 2;
-							num /= 2;
-						}
-							for (i = index - 1; i >= 0; i--)
-							{
-								printf("%d", buffer[i]);
-							}
-					}
-						break;
-					}
-				case '%':
-					lp += printf("%%");
-					break;
-				default:
-					lp += printf("%%%c", *format);
-					break;
-			}
-			}
-			else 
-			{
-				lp += printf("%c", *format);
-			}
-		format++;
-	}
-	va_end(args);
-	return (lp);
+    va_list args;
+
+    va_start(args, format);
+
+    if (format == NULL)
+    {
+        return -1;
+    }
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+
+            if (!*format)
+            {
+                return -1;
+            }
+
+            switch (*format)
+            {
+                case 'c':
+                    tf += printf("%c", va_arg(args, int));
+                    break;
+
+                case 's':
+                    tf += printf("%s", va_arg(args, char *));
+                    break;
+
+                case 'd':
+                case 'i':
+                    tf += printf("%d", va_arg(args, int));
+                    break;
+
+                case 'u':
+                    tf += printf("%u", va_arg(args, unsigned int));
+                    break;
+
+                case 'o':
+                    tf += printf("%o", va_arg(args, unsigned int));
+                    break;
+
+                case 'x':
+                    tf += printf("%ls", va_arg(args, int *));
+                    break;
+
+                case 'X':
+                    tf += printf("%ls", va_arg(args, int *));
+                    break;
+
+                case 'p':
+                    tf += printf("%p", va_arg(args, void *));
+                    break;
+
+                case 'b': {
+                    unsigned int num = va_arg(args, unsigned int);
+                    char buffer[32];
+                    int index = 0;
+
+                    if (num == 0) {
+                        tf += printf("0");
+                    }
+                    else {
+                        while (num > 0) {
+                            buffer[index++] = num % 2;
+                            num /= 2;
+                        }
+                        for (i = index - 1; i >= 0; i--) {
+                            tf += printf("%d", buffer[i]);
+                        }
+                    }
+                    break;
+                }
+
+                case '%':
+                    tf += printf("%%");
+                    break;
+
+                default:
+                    tf += printf("%%%c", *format);
+                    break;
+            }
+        }
+        else {
+            tf += printf("%c", *format);
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return tf;
 }
